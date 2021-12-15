3bb1c092ce6577d69002d8c402fd230a1dea0d0d
rasanjaya85 <rlsubasinghe85@gmail.com>
2021-12-15 19:04:17 +0530

    Flask request handling

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
+from flask import request, redirect
 
+from datetime import datetime 
 
 @app.route("/")
 def index():
@@ -28,7 +30,7 @@ friends = {
 colors = ("Red", "Blue") 
 
 # Boolean
-cool = True 
+cool = False
 
 # Classe
 class GitRemote:
@@ -36,18 +38,60 @@ class GitRemote:
         self.name = name
         self.description = description
         self.domain = domain
-
+    def pull(self):
+        return f"Pulling repo {self.name}"
     def clone(self, repo):
         return f"cloing the {repo}"
 
 
 remote = GitRemote(
-    name = "Learning Flask"
-    description = "Learn the Flask python web framework"
-    domain = "https://github.com/
+    name = "Learning Flask",
+    description = "Learn the Flask python web framework",
+    domain = "https://github.com/rasanjaya85/flask-microservice.git"
 )
+
+#function
+def repeat(x, qty=1):
+    return x * qty
+
+# date 
+date = datetime.utcnow()
+
+#html
+html = "<h1>This is HTML </h1>"
+
+#suspicious
+suspicious = "<script>alert('Never trust the user input.!')</script>"
 
 @app.route("/jinja")
 def jinja():
     my_name = "Neon"
-    return render_template("public/jinja.html", my_name=my_name)
+    return render_template("public/jinja.html", name=name, age=age, langs=langs, 
+    friends=friends, colors=colors, cool=cool, remote=remote, repeat=repeat, GitRemote=GitRemote, date=date,
+    html=html, suspicious=suspicious )
+
+@app.template_filter("clean_date")
+def clean_date(date):
+    return date.strftime("%d %b %y")
+
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        missing = []
+        for key, value in request.form.items():
+            if value == "":
+                missing.append(key)
+
+        if missing:
+            feedback = f"Missing field for {', '.join(missing) }" 
+            return render_template("public/sign_up.html", feedback=feedback)
+        else:
+            username = request.form["username"]
+            email = request.form["email"]
+            password = request.form["password"]
+            print(f"{username}, {email}, {password}")
+
+        return redirect(request.url)
+    return render_template("public/sign_up.html")
+
